108baeb1a72c622e3f4afface5037cc45a2ca2aa
cisco-home-pc <feelgeel@gmail.com>
2021-12-23 15:31:13 +0100

    change from home pc

--- store_manager_redux/screens/ManualOrderingScreen.js
+++ store_manager_redux/screens/ManualOrderingScreen.js
@@ -182,7 +182,7 @@ function ManualOrdering({navigation,route}) {
            return newStock
                     })
       // }
-      console.log("trans_obj",trans_prod);
+      console.log("playing around with the code",trans_prod);
       }
 return (
 <Screen style={styles.container}>
